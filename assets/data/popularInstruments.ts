bf6d3af31cc21985a8637413ca4570d0be7dbe24
rkterungwa16 <terungwakombol@gmail.com>
2019-12-08 11:12:50 +0100

    include popular instruments for homepage

---
+++ assets/data/popularInstruments.ts
@@ -0,0 +1,17 @@
+export const popularInstruments = [
+    'Piano',
+    'Guitar',
+    'Singing',
+    'Drums',
+    'Violin',
+    'Saxophone',
+    'Flute',
+    'Clarinet',
+    'Cello',
+    'Trombone',
+    'Harp',
+    'Trumpet',
+    'Double Bass',
+    'Ukelele',
+    'Harmonica'
+];
--- src/components/home/HomeContainer.tsx
+++ src/components/home/HomeContainer.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Banner from './home-components/Banner';
+import PopularInstruments from './home-components/PopularInstruments';
 
 class Home extends React.Component {
     render() {
         return (
             <div>
                 <Banner />
+                <PopularInstruments />
             </div>
         );
     }
