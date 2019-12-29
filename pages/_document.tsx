021fdc5a6910079051a747b2aab4391ae78430c7
Vanessa Charles Thompson <vane.charles24@gmail.com>
2019-12-29 16:47:18 -0500

    more typings fixes

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -15,14 +15,15 @@ class NabiDocument extends Document {
     })
 
     /* tslint:disable */
-    (function () {
+    !(function () {
       window.__insp = window.__insp || [];
+      const __insp =  window.__insp  || [];
       __insp.push(['wid', 417049237]);
       var ldinsp = function () {
         if (typeof window.__inspld != "undefined") return; window.__inspld = 1; var insp = document.createElement('script'); insp.type = 'text/javascript'; insp.async = true; insp.id = "inspsync"; insp.src = ('https:' == document.location.protocol ? 'https' : 'http') + '://cdn.inspectlet.com/inspectlet.js?wid=417049237&r=' + Math.floor(new Date().getTime() / 3600000); var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(insp, x);
       };
       setTimeout(ldinsp, 0);
-    })()
+    })
     /* tslint:enable */
   }
   render() {
--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,6 +1,12 @@
 export {}
 declare global {
-  interface Window { analytics: any; }
+  interface Window { 
+    analytics: any;
+    __insp: any;
+    __inspld: any;
+  }
 }
 
 window.analytics = window.analytics || {};
+window.__insp = window.__insp || {};
+window.__inspld = window.__inspld || {};
