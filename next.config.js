6f66842386bc0f96bf1e364ee0ee6ae5b0767b4c
Vanessa Charles Thompson <vane.charles24@gmail.com>
2020-01-04 12:10:58 -0500

    [NO-TICKET] en variables

--- next.config.js
+++ next.config.js
@@ -1,14 +1,17 @@
 const withSass = require('@zeit/next-sass');
 const withCSS = require('@zeit/next-css');
 const withFonts = require('next-images');
+const webpack = require('webpack');
+
 module.exports = withCSS(withSass(withFonts({
-    webpack(config, options) {
-        config.module.rules.push({
-            test: /\.(ttf|woff|woff2)$/,
-            use: {
-                loader: 'url-loader',
-            }
-        });
-        return config;
-    }
+  webpack(config, options) {
+    config.module.rules.push({
+      test: /\.(ttf|woff|woff2)$/,
+      use: {
+        loader: 'url-loader',
+      }
+    });
+    config.plugins.push(new webpack.EnvironmentPlugin(['REACT_APP_API_URL']));
+    return config;
+  }
 })));
