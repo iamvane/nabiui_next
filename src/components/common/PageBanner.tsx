b90bd83847f59ead0a6c22c1cdf1d78d525c3c27
Vanessa Charles Thompson <vane.charles24@gmail.com>
2020-01-04 22:03:56 -0500

    [NM-163] added scss files

--- src/components/common/PageBanner.tsx
+++ src/components/common/PageBanner.tsx
@@ -1,41 +1,19 @@
 import React from "react"
 
+import '../../../assets/scss/PageBanner.scss';
+
 export interface Props {
   title: string;
   description: string;
 }
 
 const Banner = (props: Props) => {
-  return (<React.Fragment>
-    <style jsx>{`
-      .banner{
-          background: #06c3e1;
-          width: 100%;
-          height: 250px;
-          display: flex;
-          flex-direction: column;
-          align-items:center;
-          justify-content: center;
-          color: white
-      }
-      .title1{
-          font-size: 50px;
-          font-weight: 700                    
-      }
-      .title2{
-          font-size: 40px;
-          font-weight: 600                    
-      }
-      `
-    }
-    </style>
-
-    <div className="banner">
-        <div className="title1">{props.title}</div>
-        <div className="title2">{props.description}</div>
+  return (
+    <div className="page-banner">
+      <div className="page-banner-title">{props.title}</div>
+      <div className="page-banner-description">{props.description}</div>
     </div>
-
-    </React.Fragment>)
+  )
 }
 
 export default Banner;
--- src/components/common/PageBannerCta.tsx
+++ src/components/common/PageBannerCta.tsx
@@ -3,6 +3,8 @@ import Link from 'next/link';
 
 import { Button } from '@material-ui/core';
 
+import '../../../assets/scss/PageBannerCta.scss';
+
 export interface Props {
   description: string;
   url: string;
@@ -10,36 +12,16 @@ export interface Props {
 }
 
 const PageBannerCta = (props: Props) => {
-  return (<React.Fragment>
-    <style jsx>{`
-      .banner{
-        background: #;
-        width: 100%;
-        height: 200px;
-        display: flex;
-        flex-direction: column;
-        align-items:center;
-        justify-content: center;
-        color: white
-      }
-      .title2{
-        font-size: 30px;
-        font-weight: 600;
-      }
-      `
-    }
-    </style>
-
-    <div className="banner nabi-background-orange">
-      <div className="title2 nabi-margin-bottom-small">{props.description}</div>
+  return (
+    <div className="page-banner-cta nabi-background-orange">
+      <div className="page-banner-cta-description nabi-margin-bottom-small">{props.description}</div>
       <Link href={props.url}>
         <a>
           <Button variant="contained" color="primary">{props.buttonText}</Button>
         </a>
       </Link>
     </div>
-
-    </React.Fragment>)
+  )
 }
 
 export default PageBannerCta;
