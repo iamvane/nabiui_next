862099c1c880448736dcb683b17aa0e963fb018d
iamvane <vane.charles24@gmail.com>
2020-01-05 21:19:57 -0500

    [NM-157] finished implementing request card
    * [NM-157] finished implementing request card
    
    * [NM-157] fixed indentation and nonewline

--- src/constants/Roles.ts
+++ src/constants/Roles.ts
@@ -1,5 +1,11 @@
 export enum Role {
-    student = 'student',
-    instructor = 'instructor',
-    parent = 'parent'
+  student = 'student',
+  instructor = 'instructor',
+  parent = 'parent'
+}
+
+export enum RoleLabels {
+  student = 'Student',
+  instructor = 'Instructor',
+  parent = 'Parent'
 }
