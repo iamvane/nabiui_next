2ec2fdeeb5708cc7b19514de8298ed58432aa4f5
Vanessa Charles Thompson <vane.charles24@gmail.com>
2020-01-07 12:25:21 -0500

    progress

---
+++ src/components/AboutUs/AboutUs.tsx
@@ -0,0 +1,108 @@
+import React from "react"
+import Link from 'next/link';
+const reactStringReplace = require('react-string-replace');
+
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import Mail from '@material-ui/icons/Mail';
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
+import {
+  parentStudentFaqs,
+  instructorFaqs,
+  linkReplace,
+  FAQItem
+} from '../../../assets/data/faqs';
+import { Role } from '../../constants/Roles';
+import PageBanner from "../common/PageBanner"
+import PageBannerCta from "../common/PageBannerCta"
+import {
+  Teammate,
+  team
+} from './constants';
+import { Routes } from '../common/constants/Routes';
+
+interface Props {
+  role: Role;
+}
+
+const teammate = (props: Teammate) => {
+  <div>
+    <Avatar src={props.image} className="nabi-margin-center" />
+    <p className="nabi-text-semibold nabi-color-nabi">{props.name}</p>
+    <p className="nabi-text-semibold nabi-color-nabi">{props.role}</p>
+    {props.linkedIn &&
+      <IconButton
+        color="primary"
+        href={props.linkedIn}
+        target="_blank"
+      >
+        <img
+        src="https://nabimusic.s3.us-east-2.amazonaws.com/assets/images/linkedin.png"
+        className="nabi-custom-button-icon"
+        alt="facebook-icon"
+        />
+      </IconButton>
+    }
+    {props.email &&
+      <a href={`mailto:${props.email}`}>
+        <Mail color="primary" />
+      </a>
+    }
+    <Typography>{props.bio}</Typography>
+  </div>
+
+}
+
+export const FAQs = (props: Props) => {
+  const [expanded, setExpanded] = React.useState<string | false>('');
+  const handleChange = (panel: string) => (event: React.ChangeEvent<{}>, newExpanded: boolean) => {
+      setExpanded(newExpanded ? panel : false);
+  };
+
+  const items: FAQItem[] = props.role === Role.instructor ? instructorFaqs : parentStudentFaqs;
+  const description = props.role === Role.instructor ? constants.descriptionInstructor : constants.descriptionParent;
+  const ctaText = props.role === Role.instructor ? constants.ctaParent : constants.ctaInstructor;
+  const ctaLink = props.role === Role.instructor ? Routes.FAQParents : Routes.FAQInstructors;
+
+  return (
+    <React.Fragment>
+      <PageBanner title={constants.pageTitle} description={description} />
+      <div className="nabi-container">
+        <div className="nabi-background-white nabi-section nabi-margin-top-large nabi-margin-bottom-large nabi-border-radius">
+          {
+            items.map((item, key) => {
+              return (
+                <ExpansionPanel key={key} expanded={expanded === `panel${key}`} onChange={handleChange(`panel${key}`)}>
+                  <ExpansionPanelSummary aria-controls={`panel${key}d-content`} id={`panel${key}d-header`} expandIcon={<ExpandMoreIcon />}>
+                    <Typography>{item.question}</Typography>
+                  </ExpansionPanelSummary>
+                  <ExpansionPanelDetails>
+                  <Typography>
+                    {
+                      item.linkText ?
+                        reactStringReplace(
+                          item.answer,
+                          linkReplace,
+                          (i: number) => (
+                            <Link key={i} href={item.linkUrl}><a target={item.targetBlank && '_blank'}>{item.linkText}</a></Link>
+                          )
+                        ):
+                        item.answer
+                      }
+                    </Typography>
+                  </ExpansionPanelDetails>
+                </ExpansionPanel>
+              )
+            })
+          }
+        </div>
+      </div>
+      <PageBannerCta description={ctaText} url={ctaLink} buttonText={constants.ctaButton} />
+    </React.Fragment>
+ )
+}
---
+++ src/components/AboutUs/constants.ts
@@ -0,0 +1,50 @@
+export interface Teammate {
+  name: string;
+  role: string;
+  image: string;
+  bio: string;
+  linkedIn?: string;
+  email?: string;
+}
+
+export const team: Teammate[] = [
+  {
+    name: 'Vanessa Charles',
+    role: 'Founder & CEO/CTO',
+    image: 'https://nabimusic.s3.us-east-2.amazonaws.com/assets/images/vanessa-charles.JPG',
+    bio: 'Vanessa was born in Caracas, Venezuela, and grew up in Carupano, a small city on the Venezuelan Caribbean coast. She moved to Boston to attend college, and got her Bachelor’s from UMass Boston. Vanessa started Nabi with the dream of bringing music education to the lives of many children. She is a former classical piano teacher and self-taught software engineer. She worked at Oracle as a Software Engineer for almost two years, and helped her brothers create Coco Mercado, Venezuela’s first and only online supermarket. Vanessa means butterfly in Greek and Nabi means butterfly in Korean.',
+    linkedIn: 'https://www.linkedin.com/in/vanessacharlesnabi/',
+    email: 'vanessa@nabimusic.com'
+  },
+  {
+    name: 'Jennie Legary',
+    role: 'Marketing Strategist',
+    image: 'https://nabimusic.s3.us-east-2.amazonaws.com/assets/images/jennie-legary.jpeg',
+    bio: 'Jennie is a mezzo-soprano opera singer with a Master in Performing Arts Management from Accademia Teatro alla Scala. She’s also a design, marketing, and business consultant, with experience in promoting classical music, real estate, and early stage startups. With her two other singer colleagues, she has co-founded Coloratura: The Opera Dolls,, a company that produces educational musical toys for children.'
+  },
+  {
+    name: 'Luis Llave',
+    role: 'Back-end Engineer',
+    image: 'https://nabimusic.s3.us-east-2.amazonaws.com/assets/images/luis-llave.png',
+    bio: 'Luis was born in Arequipa (Perú), but was living in Venezuela, where graduated as an Electrical Engineer at University of Carabobo (UC). At UC, he worked in development of a web system for student academic records and class scheduling. Also, Luis helped build English-Spanish translations in Mandriva and Mageia Linux distributions. He returned to his natal city and now can see a volcano (Misti) everyday. He\'s a fan of Morricone music too.'
+
+  },
+  {
+    name: 'Marycarmen Mora',
+    role: 'Graphic Designer',
+    image: 'https://nabimusic.s3.us-east-2.amazonaws.com/assets/images/marycarmen-mora.jpg',
+    bio: 'Marycarmen is a licensed Graphic Designer. She got her education from the University of Antonio José de Sucre and from the University of Los Andes in Venezuela. She currently lives in Mérida and works as a graphic designer and illustrator. During her free time, she likes to do aerial silk and crossfit. Marycarmen is passionate about music.'
+  },
+  {
+    name: 'Mariana Del Rosal',
+    role: 'Content Writer',
+    image: 'https://nabimusic.s3.us-east-2.amazonaws.com/assets/images/mariana-del-rosal.jpeg',
+    bio: 'Mariana studied Language and Literature at the University of Buenos Aires. She is a part-time middle school teacher and has a great time teaching teens. She lives in Buenos Aires, Argentina, with her husband, a music teacher, and their two young kids, who are growing up surrounded by all kinds of musical instruments. Mariana enjoys writing more than anything, and  in her free time, she\'s learning to play the Irish tin whistle.'
+  }
+];
+
+export const titleAbout = 'About Nabi Music';
+export const ourMission = 'Our mission is to make music education easily accessible and bring encouragement to every young musician.';
+export const titleWhatWeDo = 'What We Do';
+export const whatWeDo = 'Nabi Music provides a well-rounded music learning experience for children by allowing parents to connect with qualified instructors nearby and providing unique features that motivate parents’ involvement in their children musical path.'
+export const titleTeam = 'The Nabi Music Team';
