31ae5c8bec4373fb5e66534c03160d123aa1774f
iamvane <vane.charles24@gmail.com>
2020-01-07 15:01:41 -0500

    [NM-172] about us
    * progress
    
    * progress
    
    * [NM-172] added about us page
    
    * [NM-172] removed props
    
    * [NM-172] removed unused import

---
+++ src/components/AboutUs/AboutUs.tsx
@@ -0,0 +1,72 @@
+import React from "react"
+
+import Grid from '@material-ui/core/Grid';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+
+import '../../../assets/scss/AboutUs.scss';
+import * as constants from './constants';
+
+const teammate = (teamate : constants.Teammate) => (
+  <div>
+    <Avatar src={teamate.image} className="nabi-margin-center nabi-margin-top-medium big-avatar" />
+    <p className="nabi-text-semibold nabi-color-nabi nabi-margin-bottom-xsmall">{teamate.name}</p>
+    <Typography className="nabi-text-semibold nabi-margin-top-xsmall">{teamate.role}</Typography>
+    {teamate.linkedIn &&
+      <IconButton
+        color="primary"
+        href={teamate.linkedIn}
+        target="_blank"
+      >
+        <img
+        src="https://nabimusic.s3.us-east-2.amazonaws.com/assets/images/linkedin.png"
+        className="nabi-custom-button-icon"
+        alt="linkedin-icon"
+        />
+      </IconButton>
+    }
+    {teamate.email &&
+      <a href={`mailto:${teamate.email}`}>
+        <IconButton
+          color="primary"
+          className="nabi-margin-left-xsmall"
+        >
+          <img
+          src="https://nabimusic.s3.us-east-2.amazonaws.com/assets/images/envelope-white.png"
+          className="nabi-custom-button-icon"
+          alt="email-icon"
+          />
+        </IconButton>
+      </a>
+    }
+    <Typography className="nabi-margin-top-xsmall">{teamate.bio}</Typography>
+  </div>
+);
+
+export const AboutUs = () => {
+  return (
+    <React.Fragment>
+      <div className="about-banner nabi-color-white nabi-background-nabi nabi-text-center">
+        <p className="nabi-jennasue-title nabi-margin-bottom-xsmall nabi-margin-top-xsmall">{constants.titleAbout}</p>
+        <Grid xs={12} md={6} className="nabi-margin-center"><p className="nabi-font-large">{constants.ourMission}</p></Grid>
+      </div>
+      <div className="nabi-container nabi-text-center">
+        <div className="nabi-background-white nabi-section nabi-margin-top-large nabi-border-radius">
+          <p className="nabi-jennasue-title nabi-color-nabi nabi-margin-bottom-xsmall nabi-margin-top-xsmall">{constants.titleWhatWeDo}</p>
+          <p className="nabi-font-medium">{constants.whatWeDo}</p>
+        </div>
+      </div>
+      <div className="nabi-container nabi-text-center">
+        <div className="nabi-background-white nabi-section nabi-margin-top-small nabi-margin-bottom-large nabi-border-radius">
+          <p className="nabi-jennasue-title nabi-color-nabi nabi-margin-bottom-xsmall nabi-margin-top-xsmall">{constants.titleTeam}</p>
+          {
+            constants.team.map((item: constants.Teammate, key) => {
+              return teammate(item);
+            })
+          }
+        </div>
+      </div>
+    </React.Fragment>
+ )
+}
