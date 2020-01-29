574e515573f513e109309bc4ac1590fe8a1e0045
vickler charles <charlesvikler@gmail.com>
2020-01-28 19:28:27 -0500

    added component

---
+++ src/components/Offers/Offer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import OfferContent from "./OfferContent";
+
+const Offer = () => {
+  return (
+    <div className>
+      <OfferContent />
+    </div>
+  );
+};
+
+export default Offer;
---
+++ src/components/Offers/OfferContent.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const OfferContent = () => {
+  return (
+    <div className="special-offer">
+      <h1>hola como estas?</h1>
+    </div>
+  );
+};
+
+export default OfferContent;
