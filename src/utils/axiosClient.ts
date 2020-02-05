f5e71eda0658683b7374ad8f06dabec1f7d67dd6
vickler charles <charlesvikler@gmail.com>
2020-02-04 22:07:58 -0500

    solved conflicts

--- src/utils/axiosClient.ts
+++ src/utils/axiosClient.ts
@@ -1,7 +1,3 @@
 import axios from "axios";
 
-<<<<<<< HEAD
 axios.defaults.baseURL = process.env.NODE_ENV === 'production' ? process.env.REACT_APP_API_URL : 'https://nabiapistaging.herokuapp.com';
-=======
-axios.defaults.baseURL = 'https://nabiapistaging.herokuapp.com/';
->>>>>>> 196458d... added axios base url
