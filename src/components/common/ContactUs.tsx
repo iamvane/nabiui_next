c283b906ab26be3709a9ccb04cdd584a887ce57e
iamvane <vane.charles24@gmail.com>
2020-03-10 16:55:18 -0400

    [NO-TICKET] added HubSpot contact form
    * [NO-TICKET] added Hubspot contact form
    
    * removed unnecessary comment

--- src/components/common/ContactUs.tsx
+++ src/components/common/ContactUs.tsx
@@ -10,6 +10,25 @@ import { pageTitlesAndDescriptions } from '../common/constants/TitlesAndDescript
 interface Props {}
 
 export const ContacUs: React.StatelessComponent<Props> = (props: Props) =>  {
+  React.useEffect(() => {
+    const hbspt =  typeof window !== 'undefined' && (window as any)['hbspt'];
+
+  	const script = document.createElement('script');
+    script.src = 'https://js.hsforms.net/forms/v2.js';
+    document.body.appendChild(script);
+
+    script.addEventListener('load', () => {
+    	if(hbspt) {
+        console.log('vane')
+      	hbspt.forms.create({
+        	portalId: '7039981',
+          formId: '235c5d24-8f69-4bc7-8752-a083ae1a527a',
+          target: '#hubspotForm'
+        })
+      }
+    });
+  });
+
   return (
     <div className="nabi-container">
       <Head>
@@ -18,9 +37,8 @@ export const ContacUs: React.StatelessComponent<Props> = (props: Props) =>  {
       </Head>
       <PageTitle pageTitle={constants.pageTitle} />
       <div className="nabi-background-white nabi-section nabi-text-center">
-        <Typography>Chat with us using the chat widget at the bottom right corner of the page</Typography>
-        <Typography>or email us at:</Typography>
-        <Typography><a href="mailto:info@nabimusic.com">info@nabimusic.com</a></Typography>
+        <Typography className="nabi-text-mediumbold">Chat with us using the chat widget or fill out this form:</Typography>
+        <div id="hubspotForm"></div>
       </div>
     </div>
   );
