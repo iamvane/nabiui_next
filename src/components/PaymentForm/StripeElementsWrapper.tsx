d7184934ab8df21236500bc46b36f6199a79d2a4
Vanessa Charles Thompson <vane.charles24@gmail.com>
2020-06-03 07:52:51 -0400

    [NO-TICKET] fixed stripe key

--- src/components/PaymentForm/StripeElementsWrapper.tsx
+++ src/components/PaymentForm/StripeElementsWrapper.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Elements } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
 
-const stripePromise = loadStripe('pk_test_0bqLmpsvPKYaGFgPeTrmsh3s00hMjjwCJm');
+const stripePromise = loadStripe('pk_live_w3eJuwclLTq3awfhENpZwqpx002APtyTMh');
 
 const StripeElementsWrapper: React.FunctionComponent = ({
   children,
