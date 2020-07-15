934332d4072921bd12255f0481a4a7777d8082ad
rkterungwa16 <terungwakombol@gmail.com>
2020-07-15 12:31:28 +0200

    [NM-299] Add years of experience to build profile

--- src/components/Bio/constants.ts
+++ src/components/Bio/constants.ts
@@ -2,17 +2,20 @@ export namespace BioComponent {
   export enum Text {
     YourBio = 'Your bio',
     AddTitle = 'Add a title (ex: Fun and experienced piano teacher)',
-    TalkAboutYour = 'Talk about your experience. Be friendly, clear and professional.'
+    TalkAboutYour = 'Talk about your experience. Be friendly, clear and professional.',
+    YearsOfExperience = 'How many years of experience do you have?'
   }
 
   export enum FieldNames {
     BioTitle = 'bioTitle',
-    bioDescription = 'bioDescription'
+    bioDescription = 'bioDescription',
+    yearsOfExperience = 'yearsOfExperience'
   }
 
   export enum Ids {
     BioTitle = 'bioTitle',
-    bioDescription = 'bioDescription'
+    bioDescription = 'bioDescription',
+    yearsOfExperience = 'yearsOfExperience'
   }
 
   export enum Placeholders {
