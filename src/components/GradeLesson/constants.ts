01c2c06ad2c1652d4fad14475da0055986079316
rkterungwa16 <terungwakombol@gmail.com>
2020-07-23 08:22:08 +0200

    [NM-351] Mark lesson as no show

--- src/components/GradeLesson/constants.ts
+++ src/components/GradeLesson/constants.ts
@@ -3,17 +3,21 @@ export const studentNameSection = 'Student\'s name';
 export const instrumentSection = 'Instrument';
 export const dateOfLessonSection = 'Date of Lesson';
 export const gradeSection = 'Grade';
+export const missedLesson = 'Missed Lesson';
 export const commentsSection = 'Lesson Comments';
 export const button = 'Submit Grade';
+export const missedLessonDescription = 'If this is a no-show, check this box';
 
 export enum FieldNames {
   Date = 'date',
   Grade = 'grade',
+  Status = 'status',
   Comment = 'comment'
 }
 
 export enum Ids {
   Date = 'date',
   Grade = 'grade',
+  Status = 'status',
   Comment = 'comment'
 }
