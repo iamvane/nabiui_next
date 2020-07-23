93fdfc5a859dc0753f5201a33013f21103a7042e
rkterungwa16 <terungwakombol@gmail.com>
2020-07-23 13:26:28 +0200

    [NM-350] Fix css request list and employment

--- src/components/common/Templates/ListTemplate.tsx
+++ src/components/common/Templates/ListTemplate.tsx
@@ -55,7 +55,7 @@ export const ListTemplate: React.StatelessComponent<Props> = props => {
   };
 
   const { hasCallToAction, results } = props;
-
+  console.log('this -->>')
   const SearchSection = (): JSX.Element => {
     return (
       <div className="nabi-section-widest nabi-background-nabi nabi-margin-bottom-small">
@@ -118,8 +118,8 @@ export const ListTemplate: React.StatelessComponent<Props> = props => {
               </div>
             </Grid>
             <Grid item={true} xs={12}>
-              {results === 0 ? (
-                <div className="nabi-section-widest nabi-background-white nabi-text-center nabi-margin-top-small">
+              {results === 0  ? (
+                <div className="nabi-section-widest nabi-background-white nabi-text-center nabi-margin-top-small nabi-margin-bottom-small">
                   <Typography>
                     No results. Contact us if you need help.
                   </Typography>
