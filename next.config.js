bfd8d10c4411bf63f8c012134cdf151fd448ce8e
Luis Díaz <nerdytrustdeveloper@gmail.com>
2021-07-21 08:33:13 -0500

    Adding dependencies for chat component

--- next.config.js
+++ next.config.js
@@ -14,13 +14,15 @@ module.exports = withPWA(withSourceMaps(withCSS(withSass(withFonts({
     disable: process.env.NODE_ENV === 'development',
     register: true,
   },
+
   webpack(config, options) {
     config.module.rules.push({
-      test: /\.(ttf|woff|woff2)$/,
+      test: /\.(ttf|woff|woff2|png)$/,
       use: {
         loader: 'url-loader',
       }
     });
+
     config.plugins.push(new webpack.EnvironmentPlugin(['REACT_APP_API_URL', 'REACT_STRIPE_KEY']));
     return config;
   }
